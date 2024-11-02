91bae635b76fb416b020349965a536006efbdd5c
qaisstan <94216834+qaisstan@users.noreply.github.com>
2024-11-02 14:51:56 +0100

    React app with next.js

---
+++ components/banner.js
@@ -0,0 +1,12 @@
+const Banner = () => {
+  return (
+    <header>
+      <div>
+        <img src="./GloboLogo.png" alt="Logo of the company"></img>
+      </div>
+      <div>Providing houses all over the world</div>
+    </header>
+  );
+};
+
+export default Banner;
